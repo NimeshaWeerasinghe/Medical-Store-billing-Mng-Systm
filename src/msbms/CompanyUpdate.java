19d3911b163f241cc281bdd77d842007424df802
NimeshaWeerasinghe <nimeshaweerasinghe94@gmail.com>
2019-01-05 11:06:29 +0530

    adding frames

--- src/msbms/CompanyUpdate.java
+++ src/msbms/CompanyUpdate.java
@@ -9,12 +9,12 @@ package msbms;
  *
  * @author Hansi
  */
-public class MedicineUpdate extends javax.swing.JFrame {
+public class CompanyUpdate extends javax.swing.JFrame {
 
     /**
-     * Creates new form MedicineUpdate
+     * Creates new form CompanyUpdate
      */
-    public MedicineUpdate() {
+    public CompanyUpdate() {
         initComponents();
     }
 
@@ -27,67 +27,82 @@ public class MedicineUpdate extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jToggleButton1 = new javax.swing.JToggleButton();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         btnProName = new javax.swing.JComboBox();
         txtprice = new javax.swing.JTextField();
-        btnUpdtQntity = new javax.swing.JButton();
+        btnUpdtEmail = new javax.swing.JButton();
         txtquantity = new javax.swing.JTextField();
-        btnUpdtPrice = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tblupdate = new javax.swing.JTable();
+        btnUpdtContact = new javax.swing.JButton();
         btnback = new javax.swing.JButton();
-
-        jToggleButton1.setText("jToggleButton1");
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblCompany = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(51, 255, 255));
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
-        jLabel1.setText("Product Name");
+        jLabel1.setText("Company Name");
 
         btnProName.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         txtprice.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
 
-        btnUpdtQntity.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        btnUpdtQntity.setText("Update Quantity");
+        btnUpdtEmail.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnUpdtEmail.setText("Update Email");
 
         txtquantity.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
 
-        btnUpdtPrice.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        btnUpdtPrice.setText("Update Price");
+        btnUpdtContact.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnUpdtContact.setText("Update Contact");
+        btnUpdtContact.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdtContactActionPerformed(evt);
+            }
+        });
 
-        tblupdate.setModel(new javax.swing.table.DefaultTableModel(
+        btnback.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnback.setText("Back");
+
+        tblCompany.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        tblCompany.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "Product ID", "Product Name", "Company Name", "Product Category", "Quantity", "Price per unit", "Expire Date"
+                "Company ID", "Company Name", "Address", "E-mail", "Contact No"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Object.class
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, true, true, true, true
             };
 
             public Class getColumnClass(int columnIndex) {
                 return types [columnIndex];
             }
-        });
-        jScrollPane1.setViewportView(tblupdate);
 
-        btnback.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        btnback.setText("Back");
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tblCompany);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnback, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(882, 882, 882))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(194, 194, 194)
                 .addComponent(jLabel1)
@@ -99,17 +114,15 @@ public class MedicineUpdate extends javax.swing.JFrame {
                 .addGap(36, 36, 36)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(btnUpdtPrice, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnUpdtContact, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(373, 373, 373))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(btnUpdtQntity)
+                        .addComponent(btnUpdtEmail)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 639, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnback, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(331, 331, 331))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 639, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -121,14 +134,14 @@ public class MedicineUpdate extends javax.swing.JFrame {
                 .addGap(33, 33, 33)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtprice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnUpdtPrice))
+                    .addComponent(btnUpdtContact))
                 .addGap(30, 30, 30)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtquantity, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnUpdtQntity))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                    .addComponent(btnUpdtEmail))
+                .addGap(47, 47, 47)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
                 .addComponent(btnback)
                 .addContainerGap())
         );
@@ -153,6 +166,10 @@ public class MedicineUpdate extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnUpdtContactActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUpdtContactActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnUpdtContactActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -170,34 +187,33 @@ public class MedicineUpdate extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MedicineUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CompanyUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MedicineUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CompanyUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MedicineUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CompanyUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MedicineUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CompanyUpdate.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MedicineUpdate().setVisible(true);
+                new CompanyUpdate().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox btnProName;
-    private javax.swing.JButton btnUpdtPrice;
-    private javax.swing.JButton btnUpdtQntity;
+    private javax.swing.JButton btnUpdtContact;
+    private javax.swing.JButton btnUpdtEmail;
     private javax.swing.JButton btnback;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JToggleButton jToggleButton1;
-    private javax.swing.JTable tblupdate;
+    private javax.swing.JTable tblCompany;
     private javax.swing.JTextField txtprice;
     private javax.swing.JTextField txtquantity;
     // End of variables declaration//GEN-END:variables
